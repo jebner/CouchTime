66eafeded957965677f8cbc32df9204ec07b9833
ebner <ebner@89.206.82.253>
2015-11-26 14:28:27 +0100

    test2

--- app/js/core/controllers/NavbarCtrl.js
+++ app/js/core/controllers/NavbarCtrl.js
@@ -4,7 +4,7 @@ app.controller('NavbarCtrl', function ($scope, $http) {
     $http.get("http://netflixroulette.net/api/api.php?title=Attack%20on%20titan").success(function (response) {
         console.log("test")
         console.log(response)
-        console.log("fuer Melina")
+        console.log("fuer Melina2")
     });
 
 });
