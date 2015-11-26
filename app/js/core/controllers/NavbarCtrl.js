c8cb5a69e3dc8fa9c9e45b87164bf8c3c8117126
ebner <ebner@89.206.82.253>
2015-11-26 12:48:58 +0100

    Install additional node modules
    - angular ui bootstrap
    - bootstrap
    - testconnection to api

--- app/js/core/controllers/NavbarCtrl.js
+++ app/js/core/controllers/NavbarCtrl.js
@@ -1,3 +1,8 @@
-app.controller('NavbarCtrl', function($scope) {
+app.controller('NavbarCtrl', function($scope, $http) {
   $scope.items = ['Home', 'About', 'Contact'];
+    
+    $http.get("http://netflixroulette.net/api/api.php?title=Attack%20on%20titan").success(function(response) {
+          console.log("test")
+          console.log(response)});
 });
+
