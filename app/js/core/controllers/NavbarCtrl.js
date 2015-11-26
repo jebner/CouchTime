d572e66c43a65e29e67eb87fd412ea1bfd200db0
ebner <ebner@89.206.82.253>
2015-11-26 14:21:37 +0100

    test

--- app/js/core/controllers/NavbarCtrl.js
+++ app/js/core/controllers/NavbarCtrl.js
@@ -1,8 +1,11 @@
-app.controller('NavbarCtrl', function($scope, $http) {
-  $scope.items = ['Home', 'About', 'Contact'];
-    
-    $http.get("http://netflixroulette.net/api/api.php?title=Attack%20on%20titan").success(function(response) {
-          console.log("test")
-          console.log(response)});
+app.controller('NavbarCtrl', function ($scope, $http) {
+    $scope.items = ['Home', 'About', 'Contact'];
+
+    $http.get("http://netflixroulette.net/api/api.php?title=Attack%20on%20titan").success(function (response) {
+        console.log("test")
+        console.log(response)
+        console.log("fuer Melina")
+    });
+
 });
 
