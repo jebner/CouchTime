77bb430d2cec996e2b96d6b6a465af5052784189
ebner <ebner@89.206.82.253>
2015-11-26 17:19:49 +0100

    Show a table with title and pster

--- app/js/core/controllers/NavbarCtrl.js
+++ app/js/core/controllers/NavbarCtrl.js
@@ -1,11 +1,4 @@
 app.controller('NavbarCtrl', function ($scope, $http) {
     $scope.items = ['Home', 'About', 'Contact'];
-
-    $http.get("http://netflixroulette.net/api/api.php?title=Attack%20on%20titan").success(function (response) {
-        console.log("test")
-        console.log(response)
-        console.log("fuer Melina2")
-    });
-
 });
 
--- app/js/main/controllers/AboutCtrl.js
+++ app/js/main/controllers/AboutCtrl.js
@@ -1,3 +1,5 @@
-app.controller('AboutCtrl', ['$scope', function($scope) {
-  
+app.controller('AboutCtrl', ['$scope', '$http', function($scope, $http) {
+    $http.get("http://netflixroulette.net/api/api.php?director=Quentin%20Tarantino").success(function (response) {
+        $scope.response = response;
+    });
 }]);
